c6d2261bf8e79cd295013ab1771da6967cb34c1e
WindowsP141 <WindowsP141@Win10.com>
2019-12-28 17:37:14 +0530

    check condition

---
+++ mysql/where.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+#sql = "SELECT * FROM customers WHERE address = 'Park Lane 38'"
+sql = "SELECT * FROM customers WHERE address Like '%way%'"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
