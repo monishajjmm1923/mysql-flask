76928ca13420bf90d5e7b31adcdae8d6b1cec0ef
monishajjmm1923 <58972854+monishajjmm1923@users.noreply.github.com>
2020-02-27 12:50:22 +0530

    Update mail_otp.py

--- flask/app/mail_otp.py
+++ flask/app/mail_otp.py
@@ -8,7 +8,7 @@ mail = Mail(app)
 app.config["MAIL_SERVER"]='smtp.gmail.com'  
 app.config["MAIL_PORT"] = 465     
 app.config["MAIL_USERNAME"] = 'monijjmm@gmail.com'  
-app.config['MAIL_PASSWORD'] = 'monijeni1923'  
+app.config['MAIL_PASSWORD'] = '********'  
 app.config['MAIL_USE_TLS'] = False  
 app.config['MAIL_USE_SSL'] = True  
   
